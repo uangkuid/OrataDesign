7ad079c6669993302bf45e33f04a2ff8cc0eef2e
Oratakashi Nhamako <davidcreazy@gmail.com>
2025-09-07 21:45:27 +0700

    draft: sample app

--- composeApp/src/commonMain/kotlin/com/oratakashi/design/app/App.kt
+++ composeApp/src/commonMain/kotlin/com/oratakashi/design/app/App.kt
@@ -3,16 +3,29 @@ package com.oratakashi.design.app
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TooltipBox
+import androidx.compose.material3.TooltipDefaults
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import com.oratakashi.design.foundation.OrataAppTheme
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
@@ -20,31 +33,41 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 import oratadesign.composeapp.generated.resources.Res
 import oratadesign.composeapp.generated.resources.compose_multiplatform
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 @Preview
 fun App() {
     OrataAppTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { _root_ide_package_.com.oratakashi.design.Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
+        val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+        Scaffold(
+            modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+            topBar = {
+                LargeTopAppBar(
+                    title = { Text("Large TopAppBar", maxLines = 1, overflow = TextOverflow.Ellipsis) },
+                    navigationIcon = {
+
+                    },
+                    actions = {
+
+                    },
+                    scrollBehavior = scrollBehavior,
+                )
+            },
+            content = { innerPadding ->
+                LazyColumn(
+                    contentPadding = innerPadding,
+                    verticalArrangement = Arrangement.spacedBy(8.dp),
                 ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+                    val list = (0..75).map { it.toString() }
+                    items(count = list.size) {
+                        Text(
+                            text = list[it],
+                            style = MaterialTheme.typography.bodyLarge,
+                            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
+                        )
+                    }
                 }
-            }
-        }
+            },
+        )
     }
 }
--- composeApp/src/wasmJsMain/kotlin/com/oratakashi/design/config/PlatformConfig.wasmJs.kt
+++ composeApp/src/wasmJsMain/kotlin/com/oratakashi/design/config/PlatformConfig.wasmJs.kt
@@ -1,5 +1,8 @@
 package com.oratakashi.design.config
 
+import androidx.compose.material3.ColorScheme
+import androidx.compose.runtime.Composable
+
 /**
  * PlatformConfig is an object that contains platform-specific configurations.
  * @author oratakashi
@@ -8,5 +11,10 @@ package com.oratakashi.design.config
 actual object PlatformConfig {
     actual fun isDynamicColorAvailable(): Boolean {
         return false
+    }
+
+    @Composable
+    actual fun getDynamicColor(isDark: Boolean): ColorScheme? {
+        return null
     }
 }

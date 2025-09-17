31a84385be585a958f6d47423c7156e76c59f533
Oratakashi Nhamako <oratakashi@oratakashi.com>
2025-09-17 08:48:01 +0700

    draft: button size

--- composeApp/src/commonMain/kotlin/com/oratakashi/design/component/button/OraButtonSize.kt
+++ composeApp/src/commonMain/kotlin/com/oratakashi/design/component/button/OraButtonSize.kt
@@ -6,18 +6,42 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.oratakashi.design.foundation.OrataTheme
+import com.oratakashi.design.foundation.typography.OraTypographyKeyToken
 
 /**
  * OraButtonSize is an enum class that is used to define the size of the button.
  * @author oratakashi
  * @since 16 Sep 2025
  */
-sealed class OraButtonSize(
+enum class OraButtonSize(
     val contentPadding: PaddingValues,
     val minWidth: Dp,
     val minHeight: Dp,
     val iconSize: Dp,
     val iconSpacing: Dp,
-    val labelTextStyle: TextStyle
+    val labelTextStyle: OraTypographyKeyToken
 ) {
+    XSmall(
+        contentPadding = PaddingValues(
+            horizontal = 12.dp,
+            vertical = 6.dp
+        ),
+        minWidth = 83.dp,
+        minHeight = 32.dp,
+        iconSize = 20.dp,
+        iconSpacing = 4.dp,
+        labelTextStyle = OraTypographyKeyToken.LabelLarge
+    ),
+
+    Small(
+        contentPadding = PaddingValues(
+            horizontal = 16.dp,
+            vertical = 10.dp
+        ),
+        minWidth = 95.dp,
+        minHeight = 40.dp,
+        iconSize = 20.dp,
+        iconSpacing = 8.dp,
+        labelTextStyle = OraTypographyKeyToken.LabelLarge
+    )
 }

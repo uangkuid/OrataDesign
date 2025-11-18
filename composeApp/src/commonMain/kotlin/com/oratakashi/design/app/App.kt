d67af8a58a1049b89ec8bcf0e3f42f07d5fcd4f8
oratakashi <oratakashi@oratakashi.com>
2025-11-18 13:13:40 +0700

    chore: add adaptive layout on main app

--- composeApp/src/commonMain/kotlin/com/oratakashi/design/app/App.kt
+++ composeApp/src/commonMain/kotlin/com/oratakashi/design/app/App.kt
@@ -11,14 +11,19 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
+import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
+import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffoldDefaults
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import androidx.window.core.layout.WindowSizeClass
 import com.oratakashi.design.app.navigation.ComponentNavigation
 import com.oratakashi.design.app.navigation.FoundationNavigation
 import com.oratakashi.design.app.ui.main.component.ComponentScreen
@@ -35,46 +40,47 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
  * @author oratakashi
  * @since 16 Nov 2025
  */
-fun App() {
+fun App(
+    windowSizeClass: WindowSizeClass = currentWindowAdaptiveInfo().windowSizeClass
+) {
     OrataAppTheme {
         val navController = rememberNavController()
         val items = listOf(FoundationNavigation, ComponentNavigation)
         val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
-
-        Scaffold(
-            modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-            bottomBar = {
-                val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        NavigationSuiteScaffold(
+            navigationSuiteItems = {
                 val currentRoute = navBackStackEntry?.destination?.route
-                NavigationBar {
-                    items.forEach { screen ->
-                        NavigationBarItem(
-                            selected = currentRoute == screen.route,
-                            onClick = {
-                                if (currentRoute != screen.route) {
-                                    navController.navigate(screen) {
-                                        // Pop up to the start destination to avoid building a large back stack
-                                        popUpTo(navController.graph.startDestinationId) {
-                                            saveState = true
-                                        }
-                                        // Avoid multiple copies of the same destination when reselecting the same item
-                                        launchSingleTop = true
-                                        // Restore state when reselecting a previously selected item
-                                        restoreState = true
-                                    }
-                                }
-                            },
-                            icon = {
-                                Icon(painterResource(screen.icon), contentDescription = null)
-                            },
-                            label = { Text(screen.title()) },
-                            modifier = Modifier
-                                .testTag("NavigationBarItem_${screen.title()}")
-                        )
-                    }
+                items.forEach { screen ->
+                   item(
+                       selected = currentRoute == screen.route,
+                       onClick = {
+                            if (currentRoute != screen.route) {
+                                 navController.navigate(screen) {
+                                      // Pop up to the start destination to avoid building a large back stack
+                                      popUpTo(navController.graph.startDestinationId) {
+                                        saveState = true
+                                      }
+                                      // Avoid multiple copies of the same destination when reselecting the same item
+                                      launchSingleTop = true
+                                      // Restore state when reselecting a previously selected item
+                                      restoreState = true
+                                 }
+                            }
+                       },
+                       icon = {
+                           Icon(painterResource(screen.icon), contentDescription = null)
+                       },
+                       label = { Text(screen.title()) },
+//                       modifier = Modifier
+//                           .testTag("NavigationBarItem_${screen.title()}")
+                   )
                 }
-            }
-        ) { innerPadding ->
+            },
+            modifier = Modifier
+                .padding(vertical = 16.dp)
+                .nestedScroll(scrollBehavior.nestedScrollConnection)
+        ) {
             NavHost(
                 navController = navController,
                 startDestination = FoundationNavigation,
@@ -86,19 +92,77 @@ fun App() {
                     FoundationScreen(
                         scrollBehavior = scrollBehavior,
                         modifier = Modifier
-                            .padding(innerPadding)
                             .fillMaxSize()
                     )
                 }
                 composable<ComponentNavigation> {
                     ComponentScreen(
                         modifier = Modifier
-                            .padding(innerPadding)
                             .fillMaxSize(),
                         scrollBehavior = scrollBehavior
                     )
                 }
             }
         }
+
+//        Scaffold(
+//            modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+//            bottomBar = {
+//                val navBackStackEntry by navController.currentBackStackEntryAsState()
+//                val currentRoute = navBackStackEntry?.destination?.route
+//                NavigationBar {
+//                    items.forEach { screen ->
+//                        NavigationBarItem(
+//                            selected = currentRoute == screen.route,
+//                            onClick = {
+//                                if (currentRoute != screen.route) {
+//                                    navController.navigate(screen) {
+//                                        // Pop up to the start destination to avoid building a large back stack
+//                                        popUpTo(navController.graph.startDestinationId) {
+//                                            saveState = true
+//                                        }
+//                                        // Avoid multiple copies of the same destination when reselecting the same item
+//                                        launchSingleTop = true
+//                                        // Restore state when reselecting a previously selected item
+//                                        restoreState = true
+//                                    }
+//                                }
+//                            },
+//                            icon = {
+//                                Icon(painterResource(screen.icon), contentDescription = null)
+//                            },
+//                            label = { Text(screen.title()) },
+//                            modifier = Modifier
+//                                .testTag("NavigationBarItem_${screen.title()}")
+//                        )
+//                    }
+//                }
+//            }
+//        ) { innerPadding ->
+//            NavHost(
+//                navController = navController,
+//                startDestination = FoundationNavigation,
+//                enterTransition = { fadeIn() },
+//                exitTransition = { fadeOut() },
+//                modifier = Modifier
+//            ) {
+//                composable<FoundationNavigation>{
+//                    FoundationScreen(
+//                        scrollBehavior = scrollBehavior,
+//                        modifier = Modifier
+//                            .padding(innerPadding)
+//                            .fillMaxSize()
+//                    )
+//                }
+//                composable<ComponentNavigation> {
+//                    ComponentScreen(
+//                        modifier = Modifier
+//                            .padding(innerPadding)
+//                            .fillMaxSize(),
+//                        scrollBehavior = scrollBehavior
+//                    )
+//                }
+//            }
+//        }
     }
 }

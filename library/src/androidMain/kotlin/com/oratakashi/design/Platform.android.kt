68b940cc2609eab26b06468c60df502311ce2eeb
Oratakashi Nhamako <davidcreazy@gmail.com>
2025-12-21 23:37:44 +0700

    chore: refactor project structure

--- library/src/androidMain/kotlin/com/oratakashi/design/Platform.android.kt
+++ library/src/androidMain/kotlin/com/oratakashi/design/Platform.android.kt
@@ -1,3 +1,9 @@
 package com.oratakashi.design
 
-actual fun platform() = "Android"
+import android.os.Build
+
+class AndroidPlatform : Platform {
+    override val name: String = "Android ${Build.VERSION.SDK_INT}"
+}
+
+actual fun getPlatform(): Platform = AndroidPlatform()
--- library/src/commonMain/kotlin/com/oratakashi/design/Platform.kt
+++ library/src/commonMain/kotlin/com/oratakashi/design/Platform.kt
@@ -1,3 +1,18 @@
 package com.oratakashi.design
 
-expect fun platform(): String
+/**
+ * Platform interface provides platform-specific information.
+ * @author oratakashi
+ * @since 16 Nov 2025
+ */
+interface Platform {
+    val name: String
+}
+
+/**
+ * getPlatform returns the platform-specific implementation of the Platform interface.
+ * @author oratakashi
+ * @since 16 Nov 2025
+ * @return Platform instance for the current platform
+ */
+expect fun getPlatform(): Platform
